b2e0ed98652020958fac99555536d968c4c11135
Craimo <Craigkorir@gmail.com>
2024-09-24 22:24:47 +0300

    use of service principal

--- scripts/tera/providers.tf
+++ scripts/tera/providers.tf
@@ -13,4 +13,10 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  # Authenticate using environment variables
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+  subscription_id  = var.azure_subscription_id
 }
--- scripts/tera/variables.tf
+++ scripts/tera/variables.tf
@@ -11,3 +11,8 @@ variable location {
 variable kubernetes_version {    
   default = "1.30.3"
 }
+
+variable "azure_client_id" {}
+variable "azure_client_secret" {}
+variable "azure_tenant_id" {}
+variable "azure_subscription_id" {}
